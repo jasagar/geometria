423bb57b8306bc5d6452dd98f9c15d0c24999c9c
Jaime Sansano Garcia <jasano2@hotmail.com>
2020-02-02 03:23:45 +0100

    versión comentada

--- geometria/src/Rectangulo_JSG.java
+++ geometria/src/Rectangulo_JSG.java
@@ -1,19 +1,40 @@
 
+/**
+ * Clase Rectangulo_JSG. Calcula el área y el perímetro del rectángulo
+ * Subclase de la superclase FiguraGeometrica_JSG
+ * @author Jaime Sansano García
+ * @version 1.2
+ *
+ */
 public class Rectangulo_JSG extends FiguraGeometrica_JSG {
 	private double l1;
 	private double l2;
 	
+	/**
+	 * Constructor de la clase Rectangulo_JSG
+	 * @param tipoFigura Atributo String de la superclase
+	 * @param lG Valor double del lado mayor
+	 * @param lP Valor double del lado menor
+	 */
 	public Rectangulo_JSG(String tipoFigura, double lG, double lP) {
 		super(tipoFigura);
 		l1 = lG;
 		l2 = lP;
 	}
 
+	/**
+	 * Implementación del método abstracto de la superclase area()
+	 * @return l1 * l2 Double con el área del rectángulo
+	 */
 	@Override
 	public double area() {
 		return l1 * l2;
 	}
 	
+	/**
+	 * Implementación del método abstracto de la superclase perimetro()
+	 * @return (2 * l1) + (2 * l2) Double con el perímetro del rectángulo
+	 */
 	@Override
 	public double perimetro() {
 		return (2 * l1) + (2 * l2);
