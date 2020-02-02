c776639bf0f0d10f10e9e36c36d5394efcd75785
Jaime Sansano Garcia <jasano2@hotmail.com>
2020-02-02 03:28:46 +0100

    Siempre positivo

--- geometria/src/Circulo_JSG.java
+++ geometria/src/Circulo_JSG.java
@@ -7,6 +7,9 @@ public class Circulo_JSG extends FiguraGeometrica_JSG {
 	public Circulo_JSG(double r, String tipoFigura) {
 		super(tipoFigura);
 		radio = r;
+		if(radio < 0) {
+			radio = radio * (-1);
+		}
 	}
 
 	@Override
